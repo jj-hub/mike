243e7e227c0d332ed7fb98686884bd0f7f9bcb48
jj-hub <cocas259@hotmail.com>
2024-01-15 16:00:33 +0000

    up

---
+++ assets/js/calendar_data.js
@@ -0,0 +1,12 @@
+// Define an array to store events
+let events = [];
+events.push({
+    date: '2024-01-05',
+    title: 'Tit',
+    description: 'description'
+});
+events.push({
+    date: '2024-01-25',
+    title: 'Tit',
+    description: 'description'
+});
