75ead72b2a01bbeb8d36b5844defbbcc650b8bf5
jj-hub <cocas259@hotmail.com>
2024-01-27 02:09:47 +0000

    upd

--- assets/js/datatables.js
+++ assets/js/datatables.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
     $('.xrefs').DataTable(); // activates DataTables for xRefs class Tables
     $('#eurusd_trend').DataTable();
     $('#02_analysis').DataTable();
+    $('#roi_table').DataTable();
+    $('#generic_table1').DataTable();
+    $('#generic_table2').DataTable();
+    $('#generic_table3').DataTable();
 });
